4dfc4833beaec6f205d9b9c123c607a1be78d5ba
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-10-24 21:40:28 -0400

    Update pictohex.py

--- pictohex.py
+++ pictohex.py
@@ -6,6 +6,7 @@ def main():
 	with open(f'{gv.userin}{gv.usercountry}plot.png', 'rb') as f:
 		content = f.read()
 	gv.img = str(b.hexlify(content))
+	gv.img = gv.img[2:len(gv.img)-1]
 
 
 if __name__ == '__main__':
